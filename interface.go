833723ee0535ae5c0f36a2b3c9233d5b574b48ce
jixiuf <jixiuf@gmail.com>
2015-02-11 15:55:52 +0800

    add comment about param of NewRedisLogger

--- interface.go
+++ interface.go
@@ -9,6 +9,8 @@ type RedisLogger interface {
 	FlushAll()
 }
 
+// maxCacheCnt 某个key 的队列长度达到此值后会强制批量把其刷到redis中(如果想每次AddLog 直接刷到redis，达设置此值为1即可)
+// idleFlushIntervalSeconds 空闲时间达到此值后会强制批量把其刷到redis中
 func NewRedisLogger(client redisapi.Redis, maxCacheCnt int, idleFlushIntervalSeconds int) RedisLogger {
 	logger := &RedisLoggerImpl{client: client,
 		logMgrMap:                make(map[string]*LogMgr),
